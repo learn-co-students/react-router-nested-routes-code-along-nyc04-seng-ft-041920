8c67c06a5c51f47a3395dd3937542a6832855937
senadakadric <skadric@fordham.edu>
2020-07-23 09:23:17 -0400

    Done.

--- src/containers/MoviesPage.js
+++ src/containers/MoviesPage.js
@@ -1,12 +1,22 @@
 // .src/containers/MoviesPage.js
-import React from 'react';
-import { Route } from 'react-router-dom';
-import MoviesList from '../components/MoviesList';
+import React from "react";
+import { Route } from "react-router-dom";
+import MoviesList from "../components/MoviesList";
+import MovieShow from "../components/MovieShow";
 
 const MoviesPage = ({ match, movies }) => (
   <div>
     <MoviesList movies={movies} />
+    <Route
+      exact
+      path={match.url}
+      render={() => <h3>Choose a movie from the list above plz</h3>}
+    />
+    <Route
+      path={`${match.url}/:movieID`}
+      render={(routerProps) => <MovieShow {...routerProps} movies={movies} />}
+    />
   </div>
-)
+);
 
-export default MoviesPage
+export default MoviesPage;
